e406ef688d04cf156ef9f3d31631fad1d91773f6
Harris <haf094@usask.ca>
2023-03-26 10:43:33 -0600

    added instruction enum and logical operations

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -2,6 +2,7 @@ pub struct Cpu {
   // Link Register stores the return address to come to after a subroutine call using the BL instruction
   pub r14_link_register: u23,
   pub r15_program_counter: u32,
+  pub current_program_status_register: u32,
   /*  
   Register 13 is sometimes a stack pointer 
     if the CPU is in THUMB mode, r13 is a stack pointer
@@ -19,8 +20,32 @@ impl Cpu {
     }
   }
 
-  // Dunno what a good interface to this class would be really 
-  pub fn interpet(&mut self, instuctions: Vec<u32>){
+  pub fn doDataOp(op: Instruction, op1: u32, op2: u32, ) {
+    match op {
+      AND | TST => {
+        op1 & op2
+      }
+      EOR | TEQ => {
+        op1 ^ op2
+      }
+      ORR => {
+        op1 | op2
+      }
+      MOV => {
+        op2
+      }
+      MVN => {
+        !op2
+      }
+      BIC => {
+        op1 & !(op2)
+      }
+      _ => todo!()
+    }
+  }
 
+  // Dunno what a good interface to this class would be really 
+  pub fn step(&mut self, instuction: u32){
+    let instructionType = self.determineType(instruction);
   }
 }
---
+++ src/cpu/instruction.rs
@@ -0,0 +1,18 @@
+enum Instruction {
+  AND, 
+  EOR, 
+  SUB, 
+  RSB, 
+  ADD, 
+  ADC, 
+  SBC, 
+  RSC, 
+  TST, 
+  CMP, 
+  CMN, 
+  ORR, 
+  MOV, 
+  BIC, 
+  MVN
+}
+
