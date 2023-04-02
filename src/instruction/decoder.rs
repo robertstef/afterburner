73159d2a26a5338613c41bae42cc9224936438f2
Robert Stefanyshin <robertstefanyshin@gmail.com>
2023-04-02 15:17:47 -0600

    Implemeted generic decode algorithm for decoding all instruction types.

--- src/instruction/decoder.rs
+++ src/instruction/decoder.rs
@@ -1,47 +1,29 @@
-use crate::instruction::instruction::ALUInstruction;
-pub struct DecodeOperation {
-    pub mask: u32,
-    pub shift_amt: u32,
-}
-pub fn decode_alu_instruction(instruction: u32, operations: &Vec<DecodeOperation>) -> ALUInstruction {
-    let decoded_fields = decode(instruction, operations);
-    // TODO robert fix this awful code 
-    ALUInstruction { 
-        condition: (decoded_fields[0] as u8), 
-        is_immediate: (decoded_fields[1] != 0), 
-        op_code: (decoded_fields[2] as u8), 
-        set_cond_code: (decoded_fields[3] != 0), 
-        first_op_register: (decoded_fields[4] as u8), 
-        dest_register: (decoded_fields[5] as u8), 
-        shift_and_second_op: (decoded_fields[6] as u16) 
-    }
-}
-
-fn decode(instruction: u32, operations: &Vec<DecodeOperation>) -> Vec<u32> {
-
-    if _check_shift_amt_correct(&operations) == false {
-        panic!("Shift amount must add to 32. Check your shift amounts.")
-    }
+pub fn decode(instruction: u32, 
+              fields: &mut Vec<&mut u32>, 
+              decode_ops: &Vec<(u32, u32)>) {
     
-    let mut instruction_fields: Vec<u32> = Vec::new();
-    for op in operations {
-        let field = instruction & op.mask;
-        instruction_fields.push(field);
-        instruction >> op.shift_amt;
+    const SHIFT_AMT: u32 = 32;
+    let mut total_shift_amt = 0;
+    for (_, shift_amt) in decode_ops {
+        total_shift_amt += shift_amt;
     }
-    instruction_fields
-}
 
-fn _check_shift_amt_correct(operations: &Vec<DecodeOperation>) -> bool {
-    let SHIFT_AMT = 32;
-    let mut tot = 0;
-    for op in operations {
-        tot += op.shift_amt
+    if total_shift_amt != SHIFT_AMT { 
+        panic!("Shift amount must add to 32 but instead added to {total_shift_amt}")
     }
-    
-    tot == SHIFT_AMT
-}
 
+    if fields.len() != decode_ops.len() {
+        panic!("Number of fields and decode operations is not equal. There must be one field \
+                for each decode operation.")
+    }
+
+    let mut mut_instruction = instruction;
+    for i in (0..fields.len()).rev() {
+        let (op, shift_amt) = decode_ops[i];
+        *fields[i] = mut_instruction & op;
+        mut_instruction = mut_instruction >> shift_amt;
+    }
+}
 
 
 #[cfg(test)]
@@ -50,58 +32,58 @@ pub mod tests {
 
     #[test]
     #[should_panic]
-    fn test_decode_panic() {
-        let operations = vec![
-            DecodeOperation {
-                mask: 123, 
-                shift_amt: 32
-            }, 
-            DecodeOperation {
-                mask: 234, 
-                shift_amt: 3
-            }
-        ];
-        decode(1231321321, &operations);
+    fn test_shift_amt_incorrect() {
+        let instruction: u32 = 0xF0000000;
+        let mut f1: u32 = 1;
+        let mut f2: u32 = 2;
+        let mut fields: Vec<&mut u32> = vec![&mut f1, &mut f2];
+        let decode_ops: Vec<(u32, u32)> = vec![(1, 1), (2, 2)];
+        decode(instruction, &mut fields, &decode_ops);
     }
 
     #[test]
-    fn test_ok() {
-        let operations = vec![
-            DecodeOperation {
-                mask: 0b0000_0000_0000_0000_0000_1111_1111_1111, 
-                shift_amt: 12
-            }, 
-            DecodeOperation {
-                mask: 0b0000_0000_0000_0000_1111_0000_0000_0000, 
-                shift_amt: 4
-            },
-            DecodeOperation {
-                mask: 0b0000_0000_0000_1111_0000_0000_0000_0000, 
-                shift_amt: 4
-            },
-            DecodeOperation {
-                mask: 0b0000_0000_0001_0000_0000_0000_0000_0000, 
-                shift_amt: 1
-            }, 
-            DecodeOperation {
-                mask: 0b0000_0001_1110_0000_0000_0000_0000_0000, 
-                shift_amt: 4
-            },
-            DecodeOperation {
-                mask: 0b0000_0010_0000_0000_0000_0000_0000_0000, 
-                shift_amt: 1
-            },
-            DecodeOperation {
-                mask: 0b0000_1100_0000_0000_0000_0000_0000_0000, 
-                shift_amt: 2
-            },
-            DecodeOperation {
-                mask: 0b1111_0000_0000_0000_0000_0000_0000_0000, 
-                shift_amt: 4
-            }
-        ];
-        // a semi real instruction 
-        let result = decode(0b0000_0000_0000_0000_0001_0000_0000_0010, &operations);
-        assert_eq!(result.len(), operations.len()) 
+    #[should_panic]
+    fn test_fields_decode_ops_not_equal() {
+        let instruction: u32 = 0xF0000000;
+        let mut f1: u32 = 1;
+        let mut f2: u32 = 2;
+        let mut f3: u32 = 3;
+        let mut fields: Vec<&mut u32> = vec![&mut f1, &mut f2, &mut f3];
+        let decode_ops: Vec<(u32, u32)> = vec![(1, 16), (2, 16)];
+        decode(instruction, &mut fields, &decode_ops);
     }
+
+    #[test]
+    fn test_decode_instruction() {
+        // Test instruction architecture:
+        // | 4 bits | 8 bits | 8 bits | 12 bits |
+
+        let instruction: u32 = 0xC21BDA79;
+        let mut f1_actual: u32 = 0;
+        let mut f2_actual: u32 = 0;
+        let mut f3_actual: u32 = 0;
+        let mut f4_actual: u32 = 0;
+
+        let f1_expected: u32 = 0xC;
+        let f2_expected: u32 = 0x21;
+        let f3_expected: u32 = 0xBD;
+        let f4_expected: u32 = 0xA79;
+
+        let mut fields: Vec<&mut u32> = vec![&mut f1_actual, &mut f2_actual, &mut f3_actual, &mut f4_actual];
+        let decode_ops: Vec<(u32, u32)> = vec![
+            (0xF, 4),
+            (0xFF, 8),
+            (0xFF, 8),
+            (0xFFF, 12)
+        ];
+
+        decode(instruction, &mut fields, &decode_ops);
+        
+        assert_eq!(f1_actual, f1_expected);
+        assert_eq!(f2_actual, f2_expected);
+        assert_eq!(f3_actual, f3_expected);
+        assert_eq!(f4_actual, f4_expected);
+
+    }
+
 }
